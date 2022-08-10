8157425e48c0c8f4811c16a4e1414278098d1a98
denk-ie <d.okur@hotmail.com>
2022-08-10 17:24:27 +1000

    completed level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,47 @@
-// from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+tableData.forEach(function(alienSighting) {
+    console.log(alienSighting);
+    var row = tbody.append("tr");
+    Object.entries(alienSighting).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+var button = d3.selectAll("#filter-btn");
+
+var form = d3.selectAll("form");
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+function runEnter() {
+
+    d3.event.preventDefault();
+
+    inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    console.log(inputValue);
+    console.log(tableData);
+
+    var filteredData = tableData.filter(table => table.datetime === inputValue);
+    console.log(filteredData);
+
+
+    var tbody = d3.select("tbody");
+
+    tbody.html("");
+
+    filteredData.forEach((alienSighting) => {
+        var row = tbody.append("tr");
+        Object.values(alienSighting).forEach(value => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+};
